e3f4168103d641c1ac93b8ffd18711031055d8f3
MatseVH <matse.vanhorebeek@gmail.com>
2023-01-18 15:18:48 +0100

    Commit 17

--- resources/views/components/tooltip.blade.php
+++ resources/views/components/tooltip.blade.php
@@ -2,7 +2,7 @@
     class="w-5 h-5 ml-2 cursor-pointer">
     <i class="fa-regular fa-circle-info"></i>
     <div x-show="tooltip"
-        class="absolute text-xs text-white transform translate-x-8 -translate-y-8 bg-gray-900 rounded-lg">
+        class="absolute max-w-[15rem] text-xs text-white transform translate-x-8 -translate-y-8 bg-gray-900 rounded-lg">
         <div class="w-full h-full p-2 rounded-lg bg-ksdGreen/50">
             {{ $slot }}
         </div>
